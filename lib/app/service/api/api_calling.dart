fe7ece1ce1e83f3af6a54833760a39bd2e03124e
bhargav0147 <bhargavpatel6832@gmail.com>
2024-12-20 21:26:24 +0530

    working on api call of multipart

--- lib/app/service/api/api_calling.dart
+++ lib/app/service/api/api_calling.dart
@@ -1,6 +1,8 @@
 
 import 'dart:convert';
+import 'dart:io';
 import 'package:dating_app/app/config/app_variables.dart';
+import 'package:flutter/services.dart';
 import 'package:http/http.dart' as http;
 
 class ApiService {
@@ -139,11 +141,12 @@ class ApiService {
 
 //   // POST Method With MultiPart
 
-  Future<dynamic> postMultiPartWithToken(
-      String endpoint,
-      Map<String, dynamic> data,
-      List<http.MultipartFile> imageList,
-      String token) async {
+  Future<dynamic> postMultiPartWithToken({
+    required String endpoint,
+    required Map<String, dynamic> data,
+    required List<http.MultipartFile> imageList,
+    required String token,
+  }) async {
     final uri = Uri.parse(baseUrl + endpoint);
 
     var request = http.MultipartRequest('POST', uri);
@@ -168,185 +171,30 @@ class ApiService {
 
     try {
       final response = await request.send();
+      final responseBody = await response.stream.bytesToString();
 
-      if (response.statusCode == 200 || response.statusCode == 201) {
-        final responseBody = await response.stream.bytesToString();
-        return jsonDecode(responseBody);
-      } else {
-        final errorBody = await response.stream.bytesToString();
-
-        throw Exception('Failed to upload images: $errorBody');
-      }
+      return {
+        'statusCode': response.statusCode,
+        'data': json.decode(responseBody),
+      };
     } catch (e) {
-      throw Exception('Failed to upload images: $e');
+      return {
+        'statusCode': 500,
+        'data': {'error': 'An error occurred: $e'},
+      };
     }
   }
-//
-//   // DELETE API With Token Provide
-//
-//   Future<dynamic> deleteWithToken(String endpoint, String token) async {
-//     final uri = Uri.parse(baseUrl + endpoint);
-//     final headers = {
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer $token'
-//     };
-//
-//     final response = await http.delete(uri, headers: headers);
-//
-//     if (response.statusCode == 200 || response.statusCode == 201) {
-//       return jsonDecode(response.body);
-//     } else {
-//       throw jsonDecode(response.body);
-//     }
-//   }
-//
-//   Future<dynamic> postWithToken(
-//       String endpoint, Map<String, dynamic> body, String token) async {
-//     final uri = Uri.parse(baseUrl + endpoint);
-//     final headers = {
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer $token'
-//     };
-//     final response =
-//     await http.post(uri, headers: headers, body: jsonEncode(body));
-//
-//     if (response.statusCode == 200 || response.statusCode == 201) {
-//       return jsonDecode(response.body);
-//     } else {
-//       throw jsonDecode(response.body);
-//     }
-//   }
-//
-//   // Update Profile With Image
-//
-//   Future<dynamic> updateProfileWithImageCall(
-//       {required String endpoint,
-//         required String firstName,
-//         required String lastName,
-//         required String token,
-//         File? image}) async {
-//     final uri = Uri.parse(baseUrl + endpoint);
-//
-//     final headers = {
-//       'Content-Type': 'multipart/form-data',
-//       'Authorization': 'Bearer $token'
-//     };
-//
-//     var request = http.MultipartRequest('PATCH', uri);
-//     request.headers.addAll(headers);
-//     request.fields['firstName'] = firstName;
-//     request.fields['lastName'] = lastName;
-//
-//     if (image != null) {
-//       List<int> imageBytes = await image.readAsBytes();
-//       var multipartFile = http.MultipartFile.fromBytes(
-//         'profileImage',
-//         imageBytes,
-//         filename: path.basename(image.path),
-//         contentType: MediaType('image', 'jpeg'),
-//       );
-//       request.files.add(multipartFile);
-//     }
-//
-//     try {
-//       final response = await request.send();
-//       final responseBody =
-//       await response.stream.transform(utf8.decoder).join('');
-//       return jsonDecode(responseBody);
-//     } on Exception catch (err) {
-//       return {"error": err.toString()};
-//     }
-//   }
-//
-//   // PATCH with image upload for edit box
-//
-//   Future<dynamic> putMultiPartWithToken(
-//       String endpoint,
-//       Map<String, dynamic> data,
-//       List<http.MultipartFile> imageList,
-//       String token) async {
-//     final uri = Uri.parse(baseUrl + endpoint);
-//
-//     var request = http.MultipartRequest('PUT', uri);
-//     request.headers['Content-Type'] = 'multipart/form-data';
-//     request.headers['Authorization'] = 'Bearer $token';
-//
-//     data.forEach((key, value) {
-//       if (value is String) {
-//         request.fields[key] = value;
-//       } else if (value is double || value is int) {
-//         request.fields[key] = value.toString();
-//       } else if (value is bool) {
-//         request.fields[key] = value ? 'true' : 'false';
-//       } else if (value is List || value is Map) {
-//         request.fields[key] = jsonEncode(value);
-//       } else {
-//         request.fields[key] = value.toString();
-//       }
-//     });
-//
-//     if (imageList.isNotEmpty) {
-//       request.files.addAll(imageList);
-//     }
-//
-//     try {
-//       final response = await request.send();
-//
-//       if (response.statusCode == 200 || response.statusCode == 201) {
-//         final responseBody = await response.stream.bytesToString();
-//         return jsonDecode(responseBody);
-//       } else {
-//         final errorBody = await response.stream.bytesToString();
-//         throw Exception('Failed to upload images: $errorBody');
-//       }
-//     } catch (e) {
-//       throw Exception('Failed to upload images: $e');
-//     }
-//   }
-//
-// // Get API With Token Provide
-//
-//   Future<dynamic> getWithTokenStatus(String endpoint, String token) async {
-//     final uri = Uri.parse(baseUrl + endpoint);
-//     final headers = {
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer $token'
-//     };
-//
-//     final response = await http.get(uri, headers: headers);
-//
-//     if (response.statusCode == 200 || response.statusCode == 201) {
-//       final responseBody = jsonDecode(response.body);
-//
-//       return {"statusCode": response.statusCode, "data": responseBody};
-//     } else {
-//       final responseBody = jsonDecode(response.body);
-//       throw {"statusCode": response.statusCode, "data": responseBody};
-//     }
-//   }
-//
-//   // POST API WITH STATUS
-//
-//   Future<dynamic> postWithTokenStatus(
-//       {required String endpoint,
-//         required String token,
-//         required Map<String, dynamic> body}) async {
-//     final uri = Uri.parse(baseUrl + endpoint);
-//     final headers = {
-//       'Content-Type': 'application/json',
-//       'Authorization': 'Bearer $token'
-//     };
-//
-//     final response =
-//     await http.post(uri, headers: headers, body: jsonEncode(body));
-//
-//     if (response.statusCode == 200 || response.statusCode == 201) {
-//       final responseBody = jsonDecode(response.body);
-//
-//       return {"statusCode": response.statusCode, "data": responseBody};
-//     } else {
-//       final responseBody = jsonDecode(response.body);
-//       throw {"statusCode": response.statusCode, "data": responseBody};
-//     }
-//   }
+
+  Future<http.MultipartFile> fileToMultipartFile(
+      dynamic file, String fieldName) async {
+    if (file is File) {
+      return await http.MultipartFile.fromPath(fieldName, file.path);
+    } else if (file is Uint8List) {
+      return http.MultipartFile.fromBytes(fieldName, file,
+          filename: '$fieldName.jpg');
+    } else {
+      throw Exception('Unsupported file type');
+    }
+  }
+
 }
